8f14e160cc985e1296cc861b7dbc29115e1bb77c
SoumyadipBarik735 <soumyadipbarik735@gmail.com>
2025-06-10 21:37:06 +0530

    Update personal_portfolio.js

--- personal_portfolio.js
+++ personal_portfolio.js
@@ -1,10 +1,3 @@
-// var typed=new typed(".Text", {
-//     strings:["Frontend Developer", "Web Developer", "Android Developer"],
-//     typeSpeed:100,
-//     backSpeed:100,
-//     backDelay:1000,
-//     loop:true
-// });
 document.addEventListener("DOMContentLoaded", function () {
     var typed = new Typed(".Text", {
         strings: ["Frontend Developer", "Web Developer", "Android Developer"],
