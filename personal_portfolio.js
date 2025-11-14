4f324a4f30ac0b32f55346a5ad39da262003e6b5
Soumyadip Barik <soumyadipbarik735@gmail.com>
2025-11-14 21:28:02 +0530

    first commit

---
+++ personal_portfolio.js
@@ -0,0 +1,16 @@
+// var typed=new Typed(".Text", {
+//     strings:["Frontend Developer", "Web Developer", "Android Developer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".Text", {
+        strings: ["Frontend Developer", "Web Developer", "Android Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
